51e9ac519b3074de31898c08c62c9220afc58ee4
jamesw <36804142+JamersonWalderson@users.noreply.github.com>
2021-11-01 11:46:31 -0300

    criando styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './styles.css';
 
 export default function App() {
   return (
     <div className="App">
       <h1>Olá mundo!</h1>
+      <ul>
+        <li>Aprendendo react</li>
+        <li>Aprendendo componentes</li>
+      </ul>
     </div>
   );
 }
